5d3511682764a0aedfb11b4d6f24824e87e509af
Abhinav Gujjar <abhinavgujjar@gmail.com>
2015-01-30 16:29:58 +0530

    communication with directives

--- app/app.js
+++ app/app.js
@@ -1,6 +1,64 @@
 //declaration of module
 var app = angular.module('emcApp', ['ngRoute']);
 
+app.directive('greeting', function(){
+	return {
+		restrict : 'E',
+		templateUrl: 'partials/greeting.html',
+		controller : 'greetingController'
+	}
+})
+
+app.directive('favs', function(){
+	return {
+		restrict : 'E',
+		templateUrl: 'partials/favs.html',
+		controller : 'favsController'
+	}
+})
+
+
+
+app.directive('preview', function(){
+	return {
+		restrict : 'E',
+		templateUrl: 'partials/preview.html',
+		scope : {
+			text : '=text',
+			rows : '@rows'
+		}
+	}
+})
+
+
+
+
+app.directive('hotelItem', function(){
+	return {
+		restrict : 'E',
+		templateUrl: 'partials/hotelItem.html'
+	}
+})
+
+
+app.directive('quickAdd', function(){
+	return {
+		restrict : 'E',
+		templateUrl: 'partials/quickAdd.html',
+		controller : 'addController'
+	}
+})
+
+
+
+
+app.directive('nav', function(){
+	return {
+		restrict : 'E',
+		templateUrl: 'partials/nav.html'
+	}
+})
+
 app.config(function($routeProvider){
 
 	$routeProvider.when('/home', {
